72f384fbaab4c216fdf9d1a342e9662e5d86534d
Mette <metteweyergang@gmail.com>
2019-11-27 14:15:55 +0100

    mew

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
   <div id="footer-container">
     
     <div id="oplysninger-footer">
-      <div id="las-tekst-hojre">
+      <div class="las-tekst-hojre">
           <strong>CVR nr:</strong> 12345678 <br>
         <strong>Adresse:</strong> Birk Centerpark 15 <br>
         <strong>E-mail:</strong> mutuum@hotmail.com
@@ -10,12 +10,12 @@
     </div>
     <div id="oplysninger-footer-2">
       <div id="oplysninger-footer-2-style">
-        <a id="las-tekst-venstre" href="./faq.php">FAQ</a> 
-        <a id="las-tekst-venstre" href="./handelsbetingelser.php">&nbsp;&nbsp;&nbsp;Handelsbetingelser </a> 
-        <a id="las-tekst-venstre" href="./kontakt.php" >&nbsp;&nbsp;&nbsp;Kontakt</a> 
+        <a class="las-tekst-venstre" href="./faq.php">FAQ</a> 
+        <a class="las-tekst-venstre" href="./handelsbetingelser.php">&nbsp;&nbsp;&nbsp;Handelsbetingelser </a> 
+        <a class="las-tekst-venstre" href="./kontakt.php" >&nbsp;&nbsp;&nbsp;Kontakt</a> 
     </div>
       </div>
-    <div id="billede-footer">
+    <div class="billede-footer">
         <?php
             if(isset($_SESSION['user_id'])) {
             echo '<a href="minside.php"><img id="billede" src="images/B4.png"></a>';
