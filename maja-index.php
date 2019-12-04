496a7a05e480a8a7bb8b5fb938575f3d05d10f28
Maja <xiastald@hotmail.com>
2019-12-04 12:57:06 +0100

    gem

--- maja-index.php
+++ maja-index.php
@@ -61,7 +61,7 @@ require_once('includes/header.php');
 
       <div class="col-lg-3 col-md-6 mb-4">
         <div class="card h-100">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <img class="card-img-top" src="images/l%C3%A5ngiver.PNG" alt="">
           <div class="card-body">
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
@@ -74,7 +74,7 @@ require_once('includes/header.php');
 
       <div class="col-lg-3 col-md-6 mb-4">
         <div class="card h-100">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <img class="card-img-top" src="images/l%C3%A5ntager.PNG" alt="">
           <div class="card-body">
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
