bee8a42c92514581d094e632674926ea020e91cd
Maja <xiastald@hotmail.com>
2019-12-04 22:30:49 +0100

    mere forside

--- maja-index.php
+++ maja-index.php
@@ -2,16 +2,22 @@
 $page = ('Forside');
 require_once('includes/header.php');
 ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
  <!-- Page Content -->
-  <div class="container">
+<div class="container">
 
     <!-- Jumbotron Header -->
 <header class="jumbotron my-4" id="baggrundpaaforside">
 
 <!--For nu er styler jeg inde på siden så der kan holdes overblik med det-->
         <img id="logoforside" src="images/logo.png">     
-      <p>Mutuum er et sted for alle. Der kan udlånes og lånes til hvem som helst eller til bestemt personer. <br>
+      <p>Mutuum er et sted for alle. Der kan udlånes og lånes til hvem som helst eller til bestemt personer. <br><br>
     Siden er dannet på baggrund af sikkerhed og muligheden for at få hjælp og planlægge din økonomiske situationer</p>
       <a href="opretbruger.php" class="btn btn-primary btn-lg">OPRET BRUGER</a>
 </header>
@@ -42,11 +48,10 @@ require_once('includes/header.php');
              <p class="card-text">Start din investering eller dit lån med kun ét klik. Med Mutuum behøver du hverken være økonom eller millionær for at kunne drage nytte af hjemmesiden.</p>
     </div>
   </div>
- </div>
-
+ </div>      
+     
 <!-- Container (Portfolio Section) -->
 <div id="portfolio" class="container-fluid text-center bg-grey">
-  
   <h2>Tanken bag MUTUUM</h2>
   <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
     <!-- Indicators -->
@@ -67,24 +72,35 @@ require_once('includes/header.php');
       <div class="item">
         <h4>"Visionen er at lave en app, der er professionel med sikkerhed i højsædet vha. samarbejdspartnere såsom nemID, Likvido etc."<br><span>Xander, opdragsgiver</span></h4>
       </div>
-    </div>
+
+ <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
+ </div>
 </div>
 
 <!-- Container (Contact Section) -->
 <div id="contact" class="container-fluid bg-grey">
-  <h1 class="text-center">CONTACT</h1>
+  <h1 class="text-center">Kontakt os på følgende informationer</h1>
   <div class="row">
     <div class="col-sm-5">
-      <p>Kontak os på følgende informationer</p>
       <p><span class="glyphicon glyphicon-map-marker"></span>Birk Centerpark 15, 7400 Herning</p>
       <p><span class="glyphicon glyphicon-phone"></span> +45 12345678</p>
       <p><span class="glyphicon glyphicon-envelope"></span> mutuum@hotmail.com</p>
     </div>
-    <div class="col-sm-7 slideanim">
+    
+       <div class="centrerknap"></div> <button data-toggle="collapse" data-target="#demo" id="forsidekontakformular">Åben kontaktformular</button> </div>     
+            <div id="demo" class="collapse">
+                <div class="col-sm-7 slideanim">
       <div class="row">
         <div class="col-sm-6 form-group">
-          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+         <br><br> <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
         </div>
         <div class="col-sm-6 form-group">
           <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
@@ -100,6 +116,8 @@ require_once('includes/header.php');
   </div>
 </div>
 </div>
+</div>
+      
 
 <script>
 $(document).ready(function(){
