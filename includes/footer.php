fd85fb9bd9c225acd7c7a42bd4047856c12f11f9
lashvisthule <57392308+lashvisthule@users.noreply.github.com>
2019-12-04 10:32:01 +0100

    OPDATERING

--- includes/footer.php
+++ includes/footer.php
@@ -1,63 +1,15 @@
-<footer id="footer" class="footer-1">
-<div class="main-footer widgets-dark typo-light">
-<div class="container">
-<div class="row">
+ <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
 
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<div class="col-x   s-12 col-sm-6 col-md-3">
-<div class="widget subscribe no-box">
-<div class="gem-mutuum">
-<h5 class="widget-title">MUTUUM<span></span></h5>
-<p>Nemme og hurtige låneaftaler mellem 2 parter. Ingen skjulte gebyre, ingen bøvl og ingen tabte penge. </p>
-</div>
-</div>
-</div>
-
-<div class="col-xs-12 col-sm-6 col-md-3">
-<div class="widget no-box">
-<div class="gem-mutuum">
-<h5 class="widget-title">Genveje<span></span></h5>
-<ul class="thumbnail-widget">
-<li>
-<div class="thumb-content"><a href="handelsbetingelser.php">Handelsbetingelser</a></div>	
-</li>
-<li>
-<div class="thumb-content"><a href="kontakt.php">Kontaktinformationer</a></div>	
-</li>
-<li>
-<div class="thumb-content"><a href="faq.php">FAQ</a></div>	
-</li>
-<li>
-</ul>
-</div>
-</div>
-</div>
-
-<div class="col-xs-12 col-sm-6 col-md-3">
-<div class="widget no-box">
-<h5 class="widget-title">Kom i gang<span></span></h5>
-<p>Få adgang til hele Mutuum</p>
-<a id="knap" class="btn" href="opretbruger.php" target="_blank">Opret dig nu!</a>
-</div>
-</div>
-
-
-<div class="col-xs-12 col-sm-6 col-md-3">
-
-<div class="widget no-box">
-<div class="gem-mutuum">
-<h5 class="widget-title">Kontakt os<span></span></h5>
-
-<p><a href="mailto:mutuum@hotmail.com" title="glorythemes">mutuum@hotmail.com</a></p>
-<ul class="social-footer2">
-</ul>
-</div>
-</div>
-</div>
-
-</div>
-</div>
-</div>
-</footer>
 </body>
+
 </html>
