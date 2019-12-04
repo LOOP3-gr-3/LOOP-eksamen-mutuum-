63c1bc3bae5446193fc4c4cfc8911da5e73c6e6e
Maja <xiastald@hotmail.com>
2019-12-04 10:13:31 +0100

    midlertidigt

---
+++ maja-index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	   <meta charset="utf-8">
+	   <meta name="viewport" content="width=device-width, initial-scale=1">
+	   <meta name="description" content="">
+	   <meta name="author" content="Projektgruppe 3">
+        <link href="includes/styles/indexstyle.css" type="stylesheet">
+    
+<body>
+    <style id="baggrund">
+        body  {
+        background-image: url(images/bg.png);
+        }
+    </style>
+
+<hr><h1><strong>Lån sikkert og nemt</strong></h1><hr>
+    
+</body>
+    
